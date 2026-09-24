59e97dfbad4fbb00fffc90deaeba5a2ee4998712
yosephrigoni04 <yoseph.rigoni04@gmail.com>
2026-09-24 08:38:37 -0300

    feat: adiciona menu principal

---
+++ biblioteca.js
@@ -0,0 +1,39 @@
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const livro = []
+let opcao
+
+do{
+    console.log("=== Biblioteca Pessoal ===");
+    console.log("1. Cadastrar livro");
+    console.log("2. Listar livros");
+    console.log("3. Atualizar status de leitura");
+    console.log("4. Remover livro");
+    console.log("0. Sair");
+    opcao = prompt("Escolha uma opção: ")
+    switch(opcao){
+        case "1":
+            console.log("====")
+            console.log("1. Cadastrar livro")
+            break
+        case "2":
+            console.log("====")
+            console.log("2. Livros cadastrados")
+            break
+        case "3":
+            console.log("====")
+            console.log("3. Atualizar status de leitura")
+            break
+        case "4":
+            console.log("====")
+            console.log("4. Remover livro")
+            break
+        case "0":
+            console.log("====")
+            console.log("Saíndo do sistema...")
+            break
+        default:
+            console.log("Digite uma opção válida")
+    }
+}while(opcao !== "0")
