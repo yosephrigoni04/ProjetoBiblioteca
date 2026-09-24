3dd0becfc89e6500edddb8fd2967dc7e3efc3075
yosephrigoni04 <yoseph.rigoni04@gmail.com>
2026-09-24 10:53:13 -0300

    feat: Adiciona listagem de livros

--- biblioteca.js
+++ biblioteca.js
@@ -1,25 +1,36 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-const livros = []
 let opcao
-let livro = {
-    titulo: "",
-    autor: "",
-    genero: ""
+let livros = []
+
+function cadastrarLivro(){
+    const livro = {
+        titulo: prompt("Título do livro: "),
+        autor: prompt("Autor: "),
+        genero: prompt("Gênero: "),
+        status: "quero ler"
+    }
+
+    livros.push(livro)
+
+    console.log("Livro cadastrado com sucesso!")
 }
 
-function cadastrarLivro(livro){
-    livro.titulo = prompt("Título do livro: ")
-    livro.autor = prompt("Autor: ")
-    livro.genero = prompt("Gênero: ")
-    livros.push(livro)
-    console.log("Livro cadastrado com sucesso!")
-    console.log(livros)
+function listarLivros(livros){
+    if (livros.length === 0) {
+        console.log("Nenhum livro cadastrado até agora");
+    } else {
+        for(let i = 0; i < livros.length; i++){
+            let livro = livros[i]
+
+            console.log(`${i + 1}. ${livro.status} ${livro.titulo} - ${livro.autor} - ${livro.genero}`)
+        }
+    }
 }
 
 do{
-    console.log("=== Biblioteca Pessoal ===");
+    console.log("--- Biblioteca Pessoal ---");
     console.log("1. Cadastrar livro");
     console.log("2. Listar livros");
     console.log("3. Atualizar status de leitura");
@@ -30,25 +41,32 @@ do{
         case "1":
             console.log("====")
             console.log("1. Insira as informações do livro a ser cadastrado:")
-            cadastrarLivro(livro)
+            cadastrarLivro()
+            console.log("====")
             break
         case "2":
             console.log("====")
-            console.log("2. Livros cadastrados")
+            listarLivros(livros)
+            console.log("====")
             break
         case "3":
             console.log("====")
             console.log("3. Atualizar status de leitura")
+            console.log("====")
             break
         case "4":
             console.log("====")
             console.log("4. Remover livro")
+            console.log("====")
             break
         case "0":
             console.log("====")
             console.log("Saíndo do sistema...Até logo!")
+            console.log("====")
             break
         default:
+            console.log("====")
             console.log("Digite uma opção válida")
+            console.log("====")
     }
 }while(opcao !== "0")
